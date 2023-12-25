fae6376f67588933d4b94ae778aa3e3d6eece770
ReloadingBee <justaschess@gmail.com>
2023-12-25 14:04:15 +0200

    RandomBot now moves only when it's its turn.

--- Assets/Scripts/RandomBot.cs
+++ Assets/Scripts/RandomBot.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class RandomBot : MonoBehaviour
 {
-    GameManager gameManager;
-    void Start()
-    {
-        gameManager = GameManager.instance;
-    }
-    void Update()
-    {
-        if (gameManager.gameEnded) return;
-        int move = Random.Range(0, 9); // [0, 9)
-        gameManager.Move(move);
-    }
+	GameManager gameManager;
+
+	public GameManager.Players symbol = GameManager.Players.o;
+	char player;
+	void Start()
+	{
+		gameManager = GameManager.instance;
+		player = (char)symbol;
+	}
+	void Update()
+	{
+		if (gameManager.gameEnded || gameManager.currentTurn != player) return;
+		
+		int move = Random.Range(0, 9); // [0, 9)
+		gameManager.Move(move);
+	}
 }
