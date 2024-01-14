780bdcf1ca625e1e7c2df0918e191e0892197327
ReloadingBee <118016583+ReloadingBee@users.noreply.github.com>
2024-01-14 14:53:38 +0200

    Rapid trigger for numerical inputs

--- Assets/Scripts/Settings/sNumericalArrow.cs
+++ Assets/Scripts/Settings/sNumericalArrow.cs
@@ -4,8 +4,38 @@ public class sNumericalArrow : MonoBehaviour
 {
 	public sNumericalInput parent;
 	public int valueIncrement;
+
+	public bool isMouseDown;
+
+	[SerializeField] static float rapidTriggerCooldown = 0.4f;
+	public float cooldown;
+	[Range(1, 10)] public int holdChain;
+
+	private void Update()
+	{
+		cooldown -= Time.deltaTime;
+
+		if(cooldown <= 0f)
+        {
+			cooldown = 0f;
+
+			if (!isMouseDown) return;
+			parent.value += valueIncrement;
+
+			holdChain++;
+			holdChain = Mathf.Clamp(holdChain, 1, 10);
+			cooldown = rapidTriggerCooldown / holdChain;
+        }
+	}
+
 	void OnMouseDown()
 	{
-		parent.value += valueIncrement;
+		isMouseDown = true;
+		holdChain = 0;
 	}
+    private void OnMouseUp()
+    {
+		isMouseDown = false;
+		cooldown = 0f;
+    }
 }
