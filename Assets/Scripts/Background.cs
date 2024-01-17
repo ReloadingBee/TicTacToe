834a33d34ed9beffed0a11f360b211ef45051797
ReloadingBee <justaschess@gmail.com>
2024-01-17 22:08:59 +0200

    Scrolling Background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+	Settings settings;
+	
+	public Vector3 movementDirection = new Vector3(-1f, -1f, 0);
+	public float loopDistance = 1.2f;
+
+	void Start()
+	{
+		settings = Settings.instance;
+	}
+
+	void Update()
+	{
+		if (settings.disableAnimations) return;
+		transform.position += movementDirection * Time.deltaTime;
+
+		if (Mathf.Abs(transform.position.x) > loopDistance || Mathf.Abs(transform.position.y) > loopDistance)
+		{
+			transform.position = Vector3.zero;
+		}
+	}
+}
--- Assets/Scripts/Settings/Settings.cs
+++ Assets/Scripts/Settings/Settings.cs
@@ -16,11 +16,11 @@ public class Settings : MonoBehaviour
         }
     } // Singleton
 
+    public bool disableAnimations;
+    
     public bool limitFPS;
     [Range(5, 10000)] public int targetFrameRate = 60;
     
     [Range(0f, 1f)] public float musicVolume = 1f;
     [Range(0f, 1f)] public float effectVolume = 1f;
-
-    public bool disableAnimations;
 }
