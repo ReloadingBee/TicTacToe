7f32542007bbd3b77bfe31b2878346a27afbf050
ReloadingBee <justaschess@gmail.com>
2024-01-20 19:38:22 +0200

    Ghost indicators
    Ghost indicators now appear, when you hover over an empty tile.

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -7,9 +7,15 @@ public class Tile : MonoBehaviour
 	public GameObject oPrefab;
 
 	GameObject child;
-	public bool isChildAlive;
+	bool isChildAlive;
 	const float childScale = 0.2f;
 	const float childAnimationSpeed = 5;
+
+	public bool mouseOver;
+
+	public bool isGhostAlive;
+	char ghostSymbol;
+	GameObject ghost;
 
 	public int id;
 
@@ -18,35 +24,52 @@ public class Tile : MonoBehaviour
 		game = Game.instance;
 	}
 
-	void OnMouseDown()
-	{
-		// Check if it's player's move
-		game.Move(id);
-	}
 	void Update()
 	{
+		// Destroy ghost
+		if ((isChildAlive || !mouseOver) && isGhostAlive)
+		{
+			Destroy(ghost);
+			isGhostAlive = false;
+		}
+		// Check if the ghost is outdated (someone made a move)
+		if (isGhostAlive && game.currentTurn != ghostSymbol)
+		{
+			Destroy(ghost);
+			isChildAlive = false;
+			InstantiateGhost();
+		}
+
 		if (!game.hasGameStarted) return;
+
 		if (game.IsEmpty(game.board, id))
 		{
-			if (!isChildAlive)
-				return;
-			
-			// Destroy the child
-			Destroy(child);
-			isChildAlive = false;
-			
+			if (isChildAlive)
+			{
+				// Destroy the child
+				Destroy(child);
+				isChildAlive = false;
+			}
+			else if (mouseOver && !isGhostAlive)
+			{
+				InstantiateGhost();
+			}
 			return;
 		}
-		if (isChildAlive) HandleChild();
+
+		if (isChildAlive)
+		{
+			HandleChild();
+		}
 		else
 		{
 			// Instantiate a new child
 			var symbol = game.board[id] == GameManager.Players.x.ToString() ? xPrefab : oPrefab;
 			child = Instantiate(symbol, transform);
-			
+
 			// Child scale
 			child.transform.localScale = Settings.instance.disableAnimations ? Vector3.one * childScale : Vector3.zero;
-			
+
 			isChildAlive = true;
 		}
 	}
@@ -64,5 +87,31 @@ public class Tile : MonoBehaviour
 		{
 			child.transform.localScale = Vector3.one * childScale;
 		}
+	}
+
+	void InstantiateGhost()
+	{
+		// Instantiate a new ghost
+		var symbol = game.currentTurn.ToString() == GameManager.Players.x.ToString() ? xPrefab : oPrefab;
+		ghostSymbol = game.currentTurn.ToString() == GameManager.Players.x.ToString() ? 'x' : 'o';
+		ghost = Instantiate(symbol, transform);
+		ghost.transform.localScale = Vector3.one * childScale;
+		isGhostAlive = true;
+
+		ghost.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0.5f);
+	}
+
+	void OnMouseDown()
+	{
+		// Check if it's player's move
+		game.Move(id);
+	}
+	void OnMouseOver()
+	{
+		mouseOver = true;
+	}
+	void OnMouseExit()
+	{
+		mouseOver = false;
 	}
 }
